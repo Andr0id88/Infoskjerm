b1e47949dd3ace4a7e8ae4ae19d77713d0c7d1a5
Andr0id88 <1988.hansen@gmail.com>
2019-03-05 15:15:18 +0100

    removed folder that wasent needed

--- weather/convert.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-img = Image.open("/home/pi/weather/out.jpg")
-area = (100, 150, 2350, 1960)
-cropped_img = img.crop(area)
-cropped_img.save("/home/pi/weather/weatherCropped.jpg")
