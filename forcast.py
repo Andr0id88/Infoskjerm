e027ad0a73951f819279beb71eb06c20d5217d30
Andr0id88 <1988.hansen@gmail.com>
2019-03-05 12:31:46 +0100

    Updated forcast

--- forcast.py
+++ forcast.py
@@ -1,7 +1,10 @@
 import wget
-from pdf2image import convert_from_bytes
+from pdf2image import convert_from_path
 from PIL import Image
+import os
 
+path = '/home/pi/weather/'
+imgpath = '/home/pi/Pictures/'
 
 def internet_connected(host="8.8.8.8", port=53):
 
@@ -14,24 +17,25 @@ def internet_connected(host="8.8.8.8", port=53):
     return False
 
 def getpdf():
-
     url = ("https://www.yr.no/sted/Norge/Nordland/Narvik/varsel.pdf")
-    wget.download(url, '/home/kali/pythonProjects/Infoskjerm/langvarsel.pdf')
+    wget.download(url, path + 'langvarsel.pdf')
 
 def pdfToImg():
-
-    pages = convert_from_bytes('/home/kali/pythonProjects/Infoskjerm/langvarsel.pdf', 300)
+    pages = convert_from_path(path + 'langvarsel.pdf', 300)
     for page in pages:
-            page.save('out.jpg', 'JPEG')
+            page.save(path + 'out.jpg', 'JPEG')
 
+def cropImg():
+    img = Image.open("out.jpg")
+    area = (100, 150, 2350, 1960)
+    cropped_img = img.crop(area)
+    cropped_img.save("weatherCropped.jpg")
 
-#def cropImg():
-#    img = Image.open("out.jpg")
-#    area = (100, 150, 2350, 1960)
-#    cropped_img = img.crop(area)
-#    cropped_img.save("weatherCropped.jpg")
+def move():
+    os.rename(path + "weatherCropped.jpg", imgpath + "forcast.jpg")
 
 
 getpdf()
 pdfToImg()
-#convert()
+cropImg()
+move()
--- weather/pdftoImg.py
+++
@@ -1,6 +0,0 @@
-from pdf2image import convert_from_path
-pages = convert_from_path('/home/pi/weather/varsel.pdf', 300)
-
-for page in pages:
-	page.save('/home/pi/weather/out.jpg', 'JPEG')
-
