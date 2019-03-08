a009172c7983e0bd58fa09fe21907694459d2aa8
Andr0id88 <1988.hansen@gmail.com>
2019-03-08 13:30:15 +0100

    changes

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd ~/ && rm -rf Desktop/ Documents/ Music/ Public/ Templates/ Videos/ && cd ~/Infoskjerm && mv newsAndForcast/ scripts/ tempPics/ ../ && cd ~/Downloads && \
-mv bakside\ raspberry.jpg tempPics/bakgrunn.jpg && killall tickr && cd ~/ && rm feed.py *.png output.xml rssfeed.sh screenshot.sh uptimePi \
-&& python3 feedpi.py && python3 forcastpi.py && rm -rf weather/ Downloads/ && cd ~/Infoskjerm/configs && mv desktop-items-0.conf .config/pcmanfm/LXDE-PI/ && sudo reboot ;
+mv bakside\ raspberry.jpg ~/tempPics/bakgrunn.jpg && killall tickr && cd ~/ && rm feed.py *.png output.xml rssfeed.sh screenshot.sh uptimePi \
+&& cd ~/newsAndForcast && python3 feedpi.py && python3 forcastpi.py && cd ~/ && rm -rf weather/ Downloads/ && \
+cd ~/Infoskjerm/configs && mv desktop-items-0.conf .config/pcmanfm/LXDE-PI/ && cd ~/Pictures && \
+rm weatherCropped.jpg && sudo reboot
 
