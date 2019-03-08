bfb60efd2c8750fc743f30d522f63d2dd9ec7960
Andr0id88 <1988.hansen@gmail.com>
2019-03-08 12:59:11 +0100

    Updates

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd ~/Infoskjerm/configs/ && mv .bashrc ~/
 rm -rf Desktop/ Documents/ Music/ Public/ Templates/ Videos/ ;
 cd ~/Infoskjerm && mv newsAndForcast/ scripts/ tempPics/ ../ && cd ~/Downloads && mv bakside\ raspberry.jpg tempPics/bakgrunn.jpg ;
 killall tickr && cd ~/ && rm feed.py *.png output.xml rssfeed.sh screenshot.sh uptimePi vmelding ;
+python3 feedpi.py && python3 forcastpi.py ;
 rm -rf weather/ Downloads/ ;
 cd ~/Infoskjerm/configs && mv desktop-items-0.conf .config/pcmanfm/LXDE-PI/ ;
+sudo reboot ;
+
