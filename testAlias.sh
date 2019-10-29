f9187a4a3aa43273e324da93686a41b2bd83d6e1
Andr0id88 <1988.hansen@gmail.com>
2019-10-29 15:11:18 +0100

    fixed

--- testAlias.sh
+++ testAlias.sh
@@ -7,3 +7,4 @@ echo "alias gp='git push'" >> ~/.bashrc
 echo "alias gd='git diff'" >> ~/.bashrc
 echo "alias ls='ls -hN --color=auto --group-directories'" >> ~/.bashrc
 echo "alias a='sudo apt-get'" >> ~/.bashrc
+echo "alias ai='sudo apt-get install'" >> ~/.bashrc
