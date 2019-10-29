137636702dc8b47630ce57c93ba3e4c943db9330
Andr0id88 <1988.hansen@gmail.com>
2019-10-29 15:23:05 +0100

    fixed script

--- testAlias.sh
+++ testAlias.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "alias ..=cd .." >> ~/.bashrc
 echo "alias s='bash ~/Infoskjerm/scripts/screenshot.sh'" >> ~/.bashrc
-echo "alias gs='git status" >> ~/.bashrc
+echo "alias gs='git status'" >> ~/.bashrc
 echo "alias ga='git add'" >> ~/.bashrc
 echo "alias gp='git push'" >> ~/.bashrc
 echo "alias gd='git diff'" >> ~/.bashrc
